7047d2609eaf89775ecd550b2ee3e2feb2a7d93f
matvrus <matviev@ucu.edu.ua>
2023-05-21 03:57:22 +0300

    init

---
+++ commandtofile.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Асоціативний масив з парами "ім'я файлу - команда"
+declare -A commands=(
+  ["app.yaml"]="kubectl ai 'Create a YAML manifest for deploying an application'"
+  ["app-livenessProbe.yaml"]="kubectl ai 'Create a YAML manifest for defining a liveness probe for an application'"
+  ["app-readinessProbe.yaml"]="kubectl ai 'Create a YAML manifest for defining a readiness probe for an application'"
+  ["app-volumeMounts.yaml"]="kubectl ai 'Create a YAML manifest for defining volume mounts for an application'"
+  ["app-cronjob.yaml"]="kubectl ai 'Create a YAML manifest for defining a cron job'"
+  ["app-job-rsync.yaml"]="kubectl ai 'Create a YAML manifest for defining a job to perform an rsync operation'"
+  ["app-multicontainer.yaml"]="kubectl ai 'Create a YAML manifest for defining a multi-container application'"
+  ["app-resources.yaml"]="kubectl ai 'Create a YAML manifest for defining resource limits and requests for an application'"
+  ["app-secret-env.yaml"]="kubectl ai 'Create a YAML manifest for defining environment variables from a secret for an application'"
+)
+
+# Створюємо папку "yaml", якщо її не існує
+mkdir -p yaml
+
+# Цикл для створення файлів і запису результатів
+for filename in "${!commands[@]}"
+do
+  # Отримуємо команду для поточного файлу
+  command="${commands[$filename]}"
+
+  # Виконуємо команду, пропускаючи перші два рядки, та перенаправляємо результат у файл у папку "yaml"
+  eval "$command" | tail -n +3 | sed -e '/\[?25l\[2K/,$ d' > "yaml/$filename"
+
+  # Виводимо повідомлення про виконання команди
+  echo "Команда '$command' виконана. Результат записано в файл 'yaml/$filename'."
+
+  # Додатковий роздільник між командами
+  echo "------------------------------------------------------------------------"
+done
